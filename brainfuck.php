c67c90323e198a135d7b705555c0313b92c81586
magnolia <magnolia1925pa@gmail.com>
2017-07-01 08:18:45 +0900

    change OOP.

--- brainfuck.php
+++ brainfuck.php
@@ -1,64 +1,57 @@
 <?php
-function execute($filename) {
-    try {
-        $f = fopen($filename, "r");
-        $cmd = prepare_cmd($f);
-        $array = array(0);
-        brainfuck($cmd, $array);
+
+class BrainFuck {
+    //init
+    private $cmd = '';
+    private $array = array(0);
+    //TODO: check $filename
+    public function __construct($filename) {
+        $this->setCmd(fopen($filename, 'r'));
+        $this->execute($this->cmd, $this->array);
     }
-    catch(Exception $e) { echo "ERROR: ".$e; }
-}
 
-//main
-function brainfuck($cmd, $array) {
-    $ap = 0;
-
-    for($cp = 0; $cp < count($cmd); $cp++) {
-        if($cmd[$cp] == "+") {
-            if($array[$ap] > PHP_INT_MAX) {
-                $array[$ap] = PHP_INT_MAX;
-            }
-            else {
-                $array[$ap]++;
-            }
+    public function setCmd($f) {
+        $bf_cmd = array(">", "<", "+", "-", "[", "]", ",", ".");
+        $cmd = array();
+        while(!feof($f)) {
+            $c = fgetc($f);
+            if(in_array($c, $bf_cmd)) array_push($cmd, $c);
         }
-        if($cmd[$cp] == "-") {
-            if($array[$ap] < PHP_INT_MIN) {
-                $array[$ap] = PHP_INT_MIN;
+        $this->cmd = $cmd;
+    }
+
+    //main
+    public function execute($cmd, $array) {
+        $ap = 0;
+        for($cp = 0; $cp < count($cmd); $cp++) {
+            if($cmd[$cp] == "+") {
+                if($array[$ap] > PHP_INT_MAX) {
+                    $array[$ap] = PHP_INT_MAX;
+                }
+                else {
+                    $array[$ap]++;
+                }
             }
-            else {
-                $array[$ap]--;
+            if($cmd[$cp] == "-") {
+                if($array[$ap] < PHP_INT_MIN) {
+                    $array[$ap] = PHP_INT_MIN;
+                }
+                else {
+                    $array[$ap]--;
+                }
             }
+            if($cmd[$cp] == ">") {
+                $ap++;
+                if($ap == count($array)) array_push($array, 0);
+            }
+            if($cmd[$cp] == "<") {
+                if($ap <= 0) $ap = count($array) - 1;
+                $ap--;
+            }
+            if($cmd[$cp] == ".") echo chr($array[$ap]);
+            if($cmd[$cp] == ",") echo fgetc(STDIN);
+            if($cmd[$cp] == "[" && $array[$ap] == 0) while($cmd[$cp] != "]") $cp++;
+            if($cmd[$cp] == "]" && $array[$ap] != 0) while($cmd[$cp] != "[") $cp--;
         }
-
-        if($cmd[$cp] == ">") {
-            $ap++;
-            if($ap == count($array)) array_push($array, 0);
-        }
-        if($cmd[$cp] == "<") {
-            if($ap <= 0) $ap = count($array) - 1;
-            $ap--;
-        }
-
-        if($cmd[$cp] == ".") echo chr($array[$ap]);
-        if($cmd[$cp] == ",") echo fgetc(STDIN);
-
-        if($cmd[$cp] == "[" && $array[$ap] == 0) while($cmd[$cp] != "]") $cp++;
-        if($cmd[$cp] == "]" && $array[$ap] != 0) while($cmd[$cp] != "[") $cp--;
     }
 }
-function prepare_cmd($f) {
-    $bf_cmd = array(">", "<", "+", "-", "[", "]", ",", ".");
-    $cmd = array();
-    while(!feof($f)) {
-        $c = fgetc($f);
-        if(in_array($c, $bf_cmd)) array_push($cmd, $c);
-    }
-    return $cmd;
-}
-
-if(preg_match("/bf/", $argv[1]) == 1) {
-    $filename = $argv[1];
-    execute($filename);
-}
-else { echo "Please input bf file name. try again.\n"; }
